5878fdb38e1ace9ecfb24aafe6f7117438ebe236
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-11 09:56:49 +0100

    html

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// radio
+let radio5 = document.getElementById('radioFive')
+let radio10 = document.getElementById('radioTen')
+let radio15 = document.getElementById('radioFifthteen')
+let radio20 = document.getElementById('radioTwenty')
+// score
+let youScore = document.getElementById('youScore')
+let compScore = document.getElementById('compScore')
+// input
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
+// restart
+let restart = document.getElementById('restart')
+
+
+

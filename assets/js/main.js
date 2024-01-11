c15bd56ff62c0bc7cdeed16ee957e78ac2ee6148
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-11 12:28:31 +0100

    js allmost

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,13 @@ let restart = document.getElementById('restart')
 let i = 0
 let iPlayer = 0
 let iCom = 0
+let number = 5
 
 
 const compTurn = (choice) => {
     let randomChoice = Math.round(Math.random() * 2);
     let randomPic = Math.round(Math.random() * 2);
-    i++;
+    console.log(i);
 
     if (randomChoice == 0) {
         randomChoice = 'Rock';
@@ -38,28 +39,45 @@ const compTurn = (choice) => {
     if (choice == randomChoice) {
         output.textContent = 'Draw';
     } else {
+        counter()
+
         if (choice == rock) {
             if (randomChoice == 'Scissors') {
-                output.textContent = 'Player win';
+                output.textContent = `rock beats ${randomChoice}`;
+                console.log(choice.id);
+                iPlayer ++
+                youScore.textContent = iPlayer
+                
             } else if (randomChoice == 'Paper') {
-                output.textContent = 'Com win';
+                output.textContent = `${randomChoice} beats rock`;
+                iCom ++
+                compScore.textContent = iCom
             }
         } if (choice == paper) {
             if (randomChoice == 'Rock') {
-                output.textContent = 'Player win';
+                output.textContent = `paper beats ${randomChoice}`;
+                iPlayer ++
+                youScore.textContent = iPlayer
+                
             } else if (randomChoice == 'Scissors') {
-                output.textContent = 'Com win';
+                output.textContent = `${randomChoice} beats paper`;
+                iCom ++
+                compScore.textContent = iCom
             }
         } if (choice == scissors) {
             if (randomChoice == 'Paper') {
-                output.textContent = 'Player win';
+                output.textContent = `scissors beats ${randomChoice}`;
+                iPlayer ++
+                youScore.textContent = iPlayer
             } else if (randomChoice == 'Rock') {
-                output.textContent = 'Com Win';
+                output.textContent = `${randomChoice} beats scissors`;
+                iCom ++
+                compScore.textContent = iCom
             }
         }
-    }
+    } radioChoice()
 }
-compTurn()
+
 
 rock.addEventListener('click', function (){
     compTurn(rock)
@@ -71,22 +89,83 @@ scissors.addEventListener('click', function (){
     compTurn(scissors)
 })
 
+const restartFunction = () =>{
+    iPlayer = 0
+    youScore.textContent = iPlayer
+    iCom = 0
+    compScore.textContent = iCom
+    i = 0
+    output.textContent = "Let's play"
+    radio.innerHTML = `
+    <input type="radio" name="radio" id="radioFive">
+    <label for="five">5</label>
+    <input type="radio" name="radio" id="radioTen">
+    <label for="ten">10</label>
+    <input type="radio" name="radio" id="radioFifthteen">
+    <label for="fifthteen">15</label>
+    <input type="radio" name="radio" id="radioTwenty">
+    <label for="twenty">20</label>`
+}
+
+
 
 restart.addEventListener('click', () =>{
-    compTurn()
+    restartFunction()
 })
 
 const radioChoice = () => {
-    if (radio5.checked && i <= 5){
-        compTurn() 
+    if (radio5.checked){
+        number = 5
+        if(i == 5 && iPlayer > iCom){
+            console.log("OHHHHHHH");
+            restartFunction()
+            output.textContent  = "Player wins"
+
+        } else if (i == 5 && iCom > iPlayer){
+            output.textContent  = "Com wins"
+        }
     }else if(radio10.checked){
-        compTurn()
+        number = 10
+        if(i == 10 && iPlayer > iCom){
+            console.log("OHHHHHHH");
+            restartFunction()
+            output.textContent  = "Player wins"
+
+        } else if (i == 10 && iCom > iPlayer){
+            output.textContent  = "Com wins"
+        } else{
+            output.textContent = "it's a draw"
+        }
     }else if(radio15.checked){
-        compTurn()
+        number = 15
+        if(i == 15 && iPlayer > iCom){
+            console.log("OHHHHHHH");
+            restartFunction()
+            output.textContent  = "Player wins"
+
+        } else if (i == 15 && iCom > iPlayer){
+            output.textContent  = "Com wins"
+        }
     }else if(radio20.checked){
-        compTurn()
+        number = 20
+        if(i == 20 && iPlayer > iCom){
+            console.log("OHHHHHHH");
+            restartFunction()
+            output.textContent  = "Player wins"
+
+        } else if (i == 20 && iCom > iPlayer){
+            output.textContent  = "Com wins"
+        }
+        else{
+            output.textContent = "it's a draw"
+        }
     }
         
+}
+
+const counter = () =>{
+    i++
+    radio.innerHTML = `<div>${i} / ${number} </div>`
 }
 
 radio.addEventListener('change', () =>{
